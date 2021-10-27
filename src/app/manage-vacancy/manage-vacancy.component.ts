bf216518981e58b94a7970cf5ea8cfe980a8ed8e
Samuel Law <samlys.law@gmail.com>
2021-10-27 23:07:57 +0800

    manage-vacancy

---
+++ src/app/manage-vacancy/manage-vacancy.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-manage-vacancy',
+  templateUrl: './manage-vacancy.component.html',
+  styleUrls: ['./manage-vacancy.component.css']
+})
+export class ManageVacancyComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
