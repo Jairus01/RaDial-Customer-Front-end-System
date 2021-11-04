b9ac163d2b0078f6b30a93de45a5d57432774e58
Raphael Jun Jie Jong <raphael.jongjj@gmail.com>
2021-11-04 12:07:55 +0800

    Added Ng2SearchPipeModule import
    Need to do "npm i ng2-search-filter --save" under 1st app dir

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,64 +1,59 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+
+import { CommonModule } from "@angular/common";
+import { FormsModule } from "@angular/forms";
+// then, add these 2 imported thingy into the @NgModule import part
+
+// for interating with backend stuff
+import { HttpClientModule } from "@angular/common/http";
+// then, import into the @NgModule import part
+// this is then connected to the backend, using REST
+
 import { AppRoutingModule } from './app-routing.module';
-import { MatToolbarModule } from '@angular/material/toolbar'; //Angular Material Toolbar Module
-import { MatButtonModule } from '@angular/material/button';
-import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
-
-
 import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { TopNavBarComponent } from './Components/Shared/top-nav-bar/top-nav-bar.component';
-import { SideNavBarComponent } from './Components/Shared/side-nav-bar/side-nav-bar.component';
-import { SubscribeServiceComponent } from './Components/Pages/subscribe-service/subscribe-service.component';
-import { LoginComponent } from './Components/Pages/login/login.component';
-import { RegisterComponent } from './Components/Pages/register/register.component';
-import { ResetPasswordComponent } from './Components/Pages/reset-password/reset-password.component';
-import { CvListingComponent } from './Components/Pages/cv-listing/cv-listing.component';
-import { DashboardComponent } from './Components/Pages/dashboard/dashboard.component';
-import { PostVacancyComponent } from './Components/Pages/post-vacancy/post-vacancy.component';
-import { ManageVacancyComponent } from './Components/Pages/manage-vacancy/manage-vacancy.component';
-import { ReceivedApplicationsComponent } from './Components/Pages/received-applications/received-applications.component';
-import { ManageHiringCampaignComponent } from './Components/Pages/manage-hiring-campaign/manage-hiring-campaign.component';
-import { VacancyShortlistComponent } from './Components/Pages/manage-hiring-campaign/vacancy-shortlist/vacancy-shortlist.component';
-import { InterviewScheduleComponent } from './Components/Pages/manage-hiring-campaign/interview-schedule/interview-schedule.component';
-import { WalletComponent } from './Components/Pages/wallet/wallet.component';
-import { SettingComponent } from './Components/Pages/setting/setting.component';
+import { HomeComponent } from './home/home.component';
+import { BooksComponent } from './books/books.component';
+import { CvComponent } from './cv/cv.component';
+import { FarmComponent } from './farm/farm.component';
+import { SiloComponent } from './farm/silo/silo.component';
+import { CvCardComponent } from './cv-card/cv-card.component';
+import { CvFilterComponent } from './cv-filter/cv-filter.component';
+import { TestModalComponent } from './test-modal/test-modal.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
-import { HomepageComponent } from './Components/Pages/homepage/homepage.component';
-import { DashboardChartComponent } from './Components/Shared/dashboard-chart/dashboard-chart.component';
+// import { SideNavBarComponent } from './side-nav-bar/side-nav-bar.component';
+// import { TopNavBarComponent } from './top-nav-bar/top-nav-bar.component';
 
+// import { MatToolbarModule } from '@angular/material/toolbar'; //Angular Material Toolbar Module
+// import { MatButtonModule } from '@angular/material/button';
+// import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { Ng2SearchPipeModule } from 'ng2-search-filter';
 
 @NgModule({
   declarations: [
     AppComponent,
-    TopNavBarComponent,
-    SideNavBarComponent,
-    SubscribeServiceComponent,
-    LoginComponent,
-    RegisterComponent,
-    ResetPasswordComponent,
-    CvListingComponent,
-    DashboardComponent,
-    PostVacancyComponent,
-    ManageVacancyComponent,
-    ReceivedApplicationsComponent,
-    ManageHiringCampaignComponent,
-    VacancyShortlistComponent,
-    InterviewScheduleComponent,
-    WalletComponent,
-    SettingComponent,
-    HomepageComponent,
-    DashboardChartComponent,
+    HomeComponent,
+    BooksComponent,
+    CvComponent,
+    FarmComponent,
+    SiloComponent,
+    CvCardComponent,
+    CvFilterComponent,
+    TestModalComponent,
+    // SideNavBarComponent,
+    // TopNavBarComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule,
-    MatToolbarModule,
-    MatButtonModule,
-    FontAwesomeModule,
-    NgbModule
+	FormsModule, // over here!
+	CommonModule, // over here!
+	HttpClientModule, NgbModule, // over here!
+	// BrowserAnimationsModule,
+    // MatToolbarModule,
+    // MatButtonModule
+	Ng2SearchPipeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
